e32a0371b29cdab3101ffcf528416beb53d397c5
manasprasad09 <manasprasad09@gmail.com>
2022-03-26 22:33:55 +0530

    day3 compeleted

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/home_page.dart';
+import 'package:flutter_application_1/login_page.dart';
+import 'package:flutter_application_1/pages/home_page.dart';
 
 void main() {
   runApp(Myapp());
@@ -10,9 +11,20 @@ void main() {
   
     @override
     Widget build(BuildContext context) {
-       
         return MaterialApp(
-        home: homepage(),
+        themeMode: ThemeMode.light,
+        theme: ThemeData(
+          primarySwatch: Colors.deepPurple
+        ),
+        darkTheme: ThemeData(
+        brightness: Brightness.dark
+        ),
+        initialRoute: "/home",
+        routes: {
+          "/" :(context) => loginpage(),
+          "/home":(context) => homepage(),
+          "/login":(context) => loginpage(),
+        },
          );
     }
   }
