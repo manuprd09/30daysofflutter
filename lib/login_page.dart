19fa479fd3e74cdcf90386c51b1432eef23a4d0d
manasprasad09 <manasprasad09@gmail.com>
2022-03-27 12:25:09 +0530

    day4completed

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -6,17 +6,48 @@ class loginpage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(child: Text("login page",style: TextStyle(
-        fontSize: 40,
-        color: Colors.blue,
-        fontWeight: FontWeight.bold,
-        
+      color: Colors.white,
+        child: Column(
+       children: [
+         Image.asset(
+           "assets/images/login_image.png",fit: BoxFit.cover,
+         ),
+         SizedBox(height: 20,),
+         Text("welcome",style: TextStyle(
+           fontSize: 24,
+           fontWeight: FontWeight.bold,
+         ),
+         ),
+         SizedBox(height: 20,),
+         Padding(
+           padding: const EdgeInsets.symmetric(vertical: 16.0,horizontal:32.0 ),
+           child: Column(children: [
+             TextFormField(
+           decoration: InputDecoration(
+            hintText: " enter user name",
+            labelText: " username" ,
 
-      ),
-      textScaleFactor: 2.0,
-      ),
-      ),
-      
-    );
+           ),
+         ),
+         TextFormField(
+           decoration: InputDecoration(
+            hintText: " enter password",
+            labelText: " password" ,
+            
+           ),
+         ),
+         SizedBox(height: 20,),
+         ElevatedButton(child: Text("login"),
+         style: TextButton.styleFrom(),
+         onPressed: (){
+           print("hi manu");
+         },
+         ),
+          
+           ],),
+         )
+       ], 
+      )
+      );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/login_page.dart';
 import 'package:flutter_application_1/pages/home_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(Myapp());
@@ -14,7 +15,8 @@ void main() {
         return MaterialApp(
         themeMode: ThemeMode.light,
         theme: ThemeData(
-          primarySwatch: Colors.deepPurple
+          primarySwatch: Colors.deepPurple,
+          fontFamily:GoogleFonts.lato().fontFamily,
         ),
         darkTheme: ThemeData(
         brightness: Brightness.dark
